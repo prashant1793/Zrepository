91f41dd01f405eb07b43438646c22e88d55ed663
Prashant Vyas <prashantvyas1793@gmail.com>
2020-09-01 14:37:06 +0000

    Commit :Version-2

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,6 +6,7 @@ sap.ui.define([
 	return Controller.extend("zrepository.Zrepository.controller.View1", {
 		onInit: function () {
 		//changes push for local brach --ver 1
+		//changes for local branch ver --2 
 		}
 	});
 });
